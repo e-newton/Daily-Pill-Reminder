7d8ff9807832b58d86ccfd4e762dc314559afec1
newtone <c3q1b@students.cs.ubc.ca>
2020-07-19 19:19:25 -0700

    Initial Commit

---
+++ Daily Pill Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily Pill Reminder
+//
+//  Created by Eric Newton on 2020-07-19.
+//  Copyright © 2020 Eric Newton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
